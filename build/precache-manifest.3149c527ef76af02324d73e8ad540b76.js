09b5e8845e36b658735020989803363efbc206e7
sup3rk3y <lalosapost@gmail.com>
2019-04-13 12:56:27 +0200

    Redeploy app

---
+++ build/precache-manifest.3149c527ef76af02324d73e8ad540b76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be88f1faf8e1a540b40f",
+    "url": "/AnotherApp/static/js/runtime~main.765f8b14.js"
+  },
+  {
+    "revision": "0a4cfc86b1888d603121",
+    "url": "/AnotherApp/static/js/main.43a36367.chunk.js"
+  },
+  {
+    "revision": "c90630e9cadd3cd1e2f1",
+    "url": "/AnotherApp/static/js/2.b4fad9df.chunk.js"
+  },
+  {
+    "revision": "0a4cfc86b1888d603121",
+    "url": "/AnotherApp/static/css/main.dfcc0a36.chunk.css"
+  },
+  {
+    "revision": "3add38f6ad0bb307e92a62935ab52983",
+    "url": "/AnotherApp/index.html"
+  }
+];
